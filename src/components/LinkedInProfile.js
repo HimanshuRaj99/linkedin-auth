93243878b4a175ad727b1b44cfe0eac141cbf6cf
HimanshuRaj99 <123356219+HimanshuRaj99@users.noreply.github.com>
2024-06-14 18:34:47 +0530

    added: headers

--- src/components/LinkedInProfile.js
+++ src/components/LinkedInProfile.js
@@ -1,5 +1,6 @@
 // SecondPage.js
 import React, { useEffect } from 'react';
+import qs from 'qs';
 import axios from "axios";
 import { useLocation } from 'react-router-dom';
 
@@ -8,15 +9,28 @@ const location = useLocation();
 
 const queryParams = new URLSearchParams(location.search);   
 const response_code = queryParams.get('code');
-
-  axios.post('https://www.linkedin.com/oauth/v2/accessToken', {
-  grant_type: 'authorization_code',
-  code: response_code,
-  redirect_uri: 'https%3A%2F%2Flinkedin-auth-six.vercel.app%2FLinkedInProfile',
-  client_id: '860qsgpwiuukok',
-  client_secret: 'WPL_AP1.8zbjSGTE135JdGYX.C7hlVQ==', 
-}).then((res) => res.json())
-.then((data) => {console.log(data)});
+const data = {   
+  'grant_type': 'authorization_code',
+  'code': response_code,
+  'redirect_uri': 'https%3A%2F%2Flinkedin-auth-six.vercel.app%2FLinkedInProfile',
+  'client_id': '860qsgpwiuukok',
+  'client_secret': 'WPL_AP1.8zbjSGTE135JdGYX.C7hlVQ=='}
+  
+const options = {
+  method: 'POST',
+  headers: { 'content-type': 'application/x-www-form-urlencoded' },
+  data: qs.stringify(data),
+  url : 'https://www.linkedin.com/oauth/v2/accessToken',
+};
+axios(options)
+//   axios.post('https://www.linkedin.com/oauth/v2/accessToken', {
+//   grant_type: 'authorization_code',
+//   code: response_code,
+//   redirect_uri: 'https%3A%2F%2Flinkedin-auth-six.vercel.app%2FLinkedInProfile',
+//   client_id: '860qsgpwiuukok',
+//   client_secret: 'WPL_AP1.8zbjSGTE135JdGYX.C7hlVQ==', 
+// }).then((res) => res.json())
+// .then((data) => {console.log(data)});
 
 // const fun = async () => {
 //   const response_code = await queryParams.get('code');
