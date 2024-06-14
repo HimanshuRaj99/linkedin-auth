d104019098056606fd6e705f758225236713d338
HimanshuRaj99 <123356219+HimanshuRaj99@users.noreply.github.com>
2024-06-14 16:11:15 +0530

    updated: client id

--- src/components/LinkedInButton.js
+++ src/components/LinkedInButton.js
@@ -3,7 +3,7 @@ import './LinkedInButton.css';
 
 const LinkedInButton = () => {
   const handleClick = () => {
-    window.location.href = 'https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=86zej1bkhxh7sd&redirect_uri=https%3A%2F%2Flinkedin-auth-six.vercel.app%2FLinkedInProfile&state=foobar&scope=liteprofile%20emailaddress%20w_member_social';
+    window.location.href = 'https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=860qsgpwiuukok&redirect_uri=https%3A%2F%2Flinkedin-auth-six.vercel.app%2FLinkedInProfile&state=foobar&scope=liteprofile%20emailaddress%20w_member_social';
   };
 
   return (
