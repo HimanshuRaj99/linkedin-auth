2d2abadbf66801ecee74700f3eb9dbaf02aec557
HimanshuRaj99 <123356219+HimanshuRaj99@users.noreply.github.com>
2024-06-14 17:04:44 +0530

    get: location

--- src/components/LinkedInProfile.js
+++ src/components/LinkedInProfile.js
@@ -1,12 +1,16 @@
 // SecondPage.js
 import React from 'react';
-
+import { useLocation } from 'react-router-dom';
 const LinkedInProfile = () => {
+  let location = useLocation();
   return (
+    
     <div>
       <h1>User Profile</h1>
       <a href="/">Go to Home Page</a>
+      <div>Current URL is {location.pathname}</div>
     </div>
+
   );
 }
 
